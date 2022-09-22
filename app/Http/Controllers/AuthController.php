e3906f39721305da08565bef0273829adab3a8e9
Matthieu MARTIN <codingstuff91@gmail.com>
2022-09-22 21:21:22 +0200

    Register new user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'remember_token' => Str::random(10),
+        ]);
+
+        return response()->json(['messsage' => 'Nouvel utilisateur crée']);
+
+    }
     public function login(Request $request)
     {
         $credentials = $request->validate([
