90b71d5c2f36bf880181daecae863ad0a8f6b0dd
Matthieu MARTIN <codingstuff91@gmail.com>
2022-09-25 18:41:19 +0200

    Create Profile and Particularities data factories

--- database/factories/CharacterFactory.php
+++ database/factories/CharacterFactory.php
@@ -3,7 +3,9 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use App\Models\Profile;
 use App\Models\Character;
+use App\Models\Particularity;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class CharacterFactory extends Factory
@@ -22,8 +24,8 @@ class CharacterFactory extends Factory
             'gender' => $this->faker->randomElement(['M', 'F']),
             'age' => $this->faker->numberBetween(25, 45),
             'height' => $this->faker->numberBetween(155, 195),
-            'trait_id' => 1,
-            'profile_id' => 1
+            'trait_id' => Particularity::all()->random(1)->first(),
+            'profile_id' => Profile::all()->random(1)->first()
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Profile;
 use App\Models\Character;
+use App\Models\Particularity;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +17,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Particularity::factory(10)->create();
+        Profile::factory(3)->create();
         Character::factory(5)->create();
     }
 }
