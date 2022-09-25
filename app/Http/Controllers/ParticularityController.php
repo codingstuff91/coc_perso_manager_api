996949664375ac494c1e62fd9ad3cbcce3047214
Matthieu MARTIN <codingstuff91@gmail.com>
2022-09-25 18:41:00 +0200

    Create Profile and Particularity Models and controllers

---
+++ app/Http/Controllers/ParticularityController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Particularity;
+use Illuminate\Http\Request;
+
+class ParticularityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Particularity  $particularity
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Particularity $particularity)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Particularity  $particularity
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Particularity $particularity)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Particularity  $particularity
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Particularity $particularity)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Particularity  $particularity
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Particularity $particularity)
+    {
+        //
+    }
+}
