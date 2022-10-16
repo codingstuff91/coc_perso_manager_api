35003dae5a2f7e90ab9f6d0f866525fbece7de18
Matthieu MARTIN <codingstuff91@gmail.com>
2022-10-16 13:51:04 +0200

    Admin routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AttributeController;
 use App\Http\Controllers\CharacterController;
+use App\Http\Controllers\ParticularityController;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,3 +37,9 @@ Route::prefix('character')->group(function(){
 Route::resource('characters', CharacterController::class);
 
 Route::resource('attributes', AttributeController::class);
+
+Route::prefix('admin')->middleware('isAdmin')->group(function(){
+    Route::resource('particularities', ParticularityController::class)->except('show', 'create');
+    Route::resource('capacities', CapacityController::class)->except('show', 'create');
+    Route::resource('profile', ProfileController::class)->except('show', 'create');
+});
