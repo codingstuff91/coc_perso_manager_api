ed2b01e9a9b821276211ac69add5ee047bd4fc90
Matthieu MARTIN <codingstuff91@gmail.com>
2022-10-16 13:51:25 +0200

    Add isAdmin controller

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => bcrypt('secret'), // password
             'remember_token' => Str::random(10),
+            'admin' => 0
         ];
     }
 }
